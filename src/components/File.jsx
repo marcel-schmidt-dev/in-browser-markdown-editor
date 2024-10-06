75e2ea21a1cb4333fdc30e8903bbb6628a20101b
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-06 19:12:54 +0200

    Refactor File component to use 'date' prop instead of 'title' prop

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import DocumentIcon from "../../public/images/icon-document.svg";
 
-function File({ title = "Document Name", fileName = "welcome.md" }) {
+function File({ date = "Document Name", fileName = "welcome.md" }) {
   return (
     <div className="flex gap-4 ml-6 items-center">
       <Image src={DocumentIcon} alt="DocumentIcon" width={14} height={0} />
       <div>
         <div
           className={`font-roboto-light text-xs ${
-            title === "Document Name" ? "text-gray-400" : "text-gray-500"
+            date === "Document Name" ? "text-gray-400" : "text-gray-500"
           }`}>
           {title}
         </div>
