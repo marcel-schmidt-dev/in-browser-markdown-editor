470f6020df636c23ff36a7981dcc61807f777bec
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-06 21:09:35 +0200

    Refactor Menu component to include dynamic visibility based on isMenuVisible prop

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,19 @@
 import File from "./File";
 import Switch from "./Switch";
 
-const Menu = () => {
+const Menu = ({ isMenuVisible }) => {
   return (
-    <div className="px-6 bg-gray-900 min-h-screen min-w-72 flex flex-col">
-      <div className="font-roboto-regular tracking-[2px] uppercase text-gray-500 text-sm h-[70px] flex items-center py-4">
+    <div
+      className={`bg-gray-900 min-h-screen flex flex-col max-w-72 overflow-x-hidden transition-all duration-300 ${
+        isMenuVisible ? "w-72 px-6" : "w-0"
+      }`}>
+      <div className="font-roboto-regular tracking-[2px] uppercase text-gray-500 text-sm h-[70px] flex items-center py-4 whitespace-nowrap">
         My Documents
       </div>
-      <button className="bg-orangeDefault rounded w-full py-3 text-center hover:bg-orangeHover transition-colors">
+      <button className="bg-orangeDefault rounded w-full py-3 text-center hover:bg-orangeHover transition-colors whitespace-nowrap">
         + New Document
       </button>
-      <div className="flex flex-col gap-6 pt-6 flex-1">
+      <div className="flex flex-col gap-6 pt-6 flex-1 whitespace-nowrap">
         <File date="01 April 2022" fileName="untitled-document.md" />
         <File date="01 April 2022" fileName="welcome.md" />
       </div>
