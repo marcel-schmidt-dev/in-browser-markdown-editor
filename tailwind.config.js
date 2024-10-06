b4245ecfc43bdd0a2b11a3fb5dfb19fdb15bd538
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-06 14:08:05 +0200

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        gray: {
+          100: '#FFFFFF',
+          200: '#F5F5F5',
+          300: '#E4E4E4',
+          400: '#C1C4CB',
+          500: '#7C8187',
+          600: '#5A6069',
+          700: '#35393F',
+          800: '#2B2D31',
+          900: '#1D1F22',
+          1000: '#151619',
+        },
+        orange_default: '#1DA1F2',
+        orange_hover: '#F39765'
       },
+      fontFamily: {
+        'roboto-light': ['Roboto-Light', 'sans-serif'],
+        'roboto-regular': ['Roboto-Regular', 'sans-serif'],
+        'roboto-mono-regular': ['RobotoMono-Regular', 'monospace'],
+        'roboto-slab-bold': ['RobotoSlab-Bold', 'serif'],
+        'roboto-slab-light': ['RobotoSlab-Light', 'serif'],
+        'roboto-slab-regular': ['RobotoSlab-Regular', 'serif'],
+      }
     },
   },
   plugins: [],
