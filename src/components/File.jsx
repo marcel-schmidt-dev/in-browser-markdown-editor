089742df20217bcc27a1964faa57d5b5d2f3aaaa
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-06 19:08:18 +0200

    feat: Add File component for displaying document information
    This commit adds a new component called File, which is responsible for displaying information about a document. It imports the necessary dependencies, including the DocumentIcon image from the /public/images directory. The File component receives props for the document title and file name, and renders them along with the document icon. This component is intended to be used in other parts of the application to display document information.

---
+++ src/components/File.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import DocumentIcon from "../../public/images/icon-document.svg";
+
+function File({ title = "Document Name", fileName = "welcome.md" }) {
+  return (
+    <div className="flex gap-4 ml-6 items-center">
+      <Image src={DocumentIcon} alt="DocumentIcon" width={14} height={0} />
+      <div>
+        <div
+          className={`font-roboto-light text-xs ${
+            title === "Document Name" ? "text-gray-400" : "text-gray-500"
+          }`}>
+          {title}
+        </div>
+        <div className="font-roboto-regular text-sm text-gray-100">{fileName}</div>
+      </div>
+    </div>
+  );
+}
+
+export default File;
