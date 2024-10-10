0560c478bb1477390b611c5497d7114b95c2db0d
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-10 19:15:46 +0200

    Refactor gitignore and package.json, and add firebase.js
    - Update .gitignore to exclude .vscode directory
    - Add firebase dependency to package.json
    - Add firebase.js file for Firebase configuration and exports

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFireStore } from 'firebase/firestore';
+
+const fireBaseConfig = {
+    apiKey: "AIzaSyCiIVgGmYDT_lu8uUnsU-ZYwHII2ZN_uAU",
+    authDomain: "markdown-editor-5f357.firebaseapp.com",
+    projectId: "markdown-editor-5f357",
+    storageBucket: "markdown-editor-5f357.appspot.com",
+    messagingSenderId: "560141745219",
+    appId: "1:560141745219:web:52cac1828e9ff088dee8f6"
+}
+
+const app = initializeApp(fireBaseConfig);
+const auth = getAuth(app);
+const db = getFireStore(app);
+
+export { auth, db }
