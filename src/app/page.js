801783c3a0d7e210b32503330d97dd9df6472a67
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-12 17:14:16 +0200

    Refactor Page component to fetch files from Firestore and add delete confirmation

--- src/app/page.js
+++ src/app/page.js
@@ -2,24 +2,49 @@
 import Header from "@/components/Header";
 import MarkdownEditor from "@/components/MarkDownEditor.jsx";
 import Menu from "@/components/Menu";
-import { useState } from "react";
+import DeleteConfirmation from "@/components/DeleteConfirmation";
+import { useState, useEffect, useCallback } from "react";
+import { db } from "@/firebase";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 export default function Home() {
 
   const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const [files, setFiles] = useState([]);
+  const [newFile, setNewFile] = useState('');
+  const [isDeleteConfirmationVisible, setIsDeleteConfirmationVisible] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuVisible(!isMenuVisible);
   };
 
+  const toggleDeleteConfirmation = () => {
+    setIsDeleteConfirmationVisible(!isDeleteConfirmationVisible);
+  };
+
+  const fetchFiles = useCallback(async () => {
+    const filesCollection = collection(db, 'files');
+    const filesSnapshot = await getDocs(filesCollection);
+    const filesList = filesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    setFiles(filesList);
+    console.log(filesList);
+  }, []);
+
+  useEffect(() => {
+    fetchFiles();
+
+  }, [fetchFiles])
+
+
   return (
     <div className="flex w-full">
       <div>
-        <Menu isMenuVisible={isMenuVisible} />
+        <Menu isMenuVisible={isMenuVisible} fileList={files} />
       </div>
       <div className="w-full h-screen">
-        <Header toggleMenu={toggleMenu} isMenuVisible={isMenuVisible} />
+        <Header toggleMenu={toggleMenu} isMenuVisible={isMenuVisible} onDeleteClick={toggleDeleteConfirmation} />
         <MarkdownEditor />
+        {isDeleteConfirmationVisible && <DeleteConfirmation onClose={toggleDeleteConfirmation} />}
       </div>
     </div>
   );
