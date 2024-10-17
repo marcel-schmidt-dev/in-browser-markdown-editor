e5ba8f8fefde57c3c61de4559694af356c18c823
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-17 18:05:08 +0200

    Refactor Menu component to add functionality for creating new documents

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,16 @@
 import File from "./File";
 import Switch from "./Switch";
 
-const Menu = ({ isMenuVisible, fileList, setActiveFile }) => {
+const Menu = ({ isMenuVisible, fileList, setActiveFile, setFiles }) => {
+  const addNewFile = () => {
+    const date = new Date();
+    const lastEdited = { seconds: Math.floor(date.getTime() / 1000) };
+    const newFile = { id: Date.now(), name: "unnamed", lastEdited: lastEdited, content: "" };
+
+    setFiles([newFile, ...fileList]);
+    setActiveFile(newFile);
+  };
+
   return (
     <div
       className={`bg-gray-900 min-h-screen flex flex-col max-w-72 overflow-x-hidden transition-all duration-300 ${
@@ -10,7 +19,9 @@ const Menu = ({ isMenuVisible, fileList, setActiveFile }) => {
       <div className="font-roboto-regular tracking-[2px] uppercase text-gray-500 text-sm h-[70px] flex items-center py-4 whitespace-nowrap">
         My Documents
       </div>
-      <button className="bg-orangeDefault text-white rounded w-full py-3 text-center hover:bg-orangeHover transition-colors whitespace-nowrap">
+      <button
+        onClick={addNewFile}
+        className="bg-orangeDefault text-white rounded w-full py-3 text-center hover:bg-orangeHover transition-colors whitespace-nowrap">
         + New Document
       </button>
       <div className="flex flex-col gap-6 pt-6 flex-1 whitespace-nowrap">
