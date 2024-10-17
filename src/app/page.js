39353c1ba5a7ae94502f650ce9911f55a775b4a8
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-17 18:02:53 +0200

    Refactor Page component to add default welcome file

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import Header from "@/components/Header";
 import { useState, useEffect, useCallback } from "react";
 import { db } from "@/firebase";
 import { collection, getDocs } from "firebase/firestore";
+import welcome from "./welcome";
 
 export default function Home() {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
@@ -26,41 +27,7 @@ export default function Home() {
     const filesCollection = collection(db, 'files');
     const filesSnapshot = await getDocs(filesCollection);
     let fileList = filesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    fileList.push({
-      id: 0, name: "welcome", lastEdited: { seconds: 1648844400 }, content: `# Welcome to Markdown
-
-Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents.
-
-## How to use this?
-
-1. Write markdown in the markdown editor window
-2. See the rendered markdown in the preview window
-
-### Features
-
-- Create headings, paragraphs, links, block quotes, inline-code, code blocks, and lists
-- Name and save the document to access again later
-- Choose between Light or Dark mode depending on your preference
-
-> This is an example of a block quote. If you would like to learn more about markdown syntax, you can visit this [markdown cheat sheet](https://www.markdownguide.org/cheat-sheet/).
-
-#### Headings
-
-To create a heading, add the hash sign (#) before the heading. The number of number signs you use should correspond to the heading level. You'll see in this guide that we've used all six heading levels (not necessarily in the correct way you should use headings!) to illustrate how they should look.
-
-##### Lists
-
-You can see examples of ordered and unordered lists above.
-
-###### Code Blocks
-
-This markdown editor allows for inline-code snippets, like this: \`<p>I'm inline</p>\`. It also allows for larger code blocks like this:
-
-\`\`\`
-<main>
-  <h1>This is a larger code block</h1>
-</main>
-\`\`\``});
+    fileList.push(welcome());
     setFiles(fileList);
     setActiveFile(fileList[fileList.length - 1]);
   }, []);
@@ -69,16 +36,10 @@ This markdown editor allows for inline-code snippets, like this: \`<p>I'm inline
     fetchFiles();
   }, [fetchFiles]);
 
-  useEffect(() => {
-    if (activeFile) {
-      console.log("Active file has changed:", activeFile);
-    }
-  }, [activeFile]);
-
   return (
     <div className="flex w-full overflow-x-hidden">
       <div>
-        <Menu isMenuVisible={isMenuVisible} fileList={files} setActiveFile={setActiveFile} />
+        <Menu isMenuVisible={isMenuVisible} fileList={files} setActiveFile={setActiveFile} setFiles={setFiles} />
       </div>
       <div className="w-full h-screen">
         <Header
@@ -87,7 +48,8 @@ This markdown editor allows for inline-code snippets, like this: \`<p>I'm inline
           onDeleteClick={toggleDeleteConfirmation}
           activeFileName={activeFile?.name}
         />
-        <MarkdownEditor activeFileContent={activeFile?.content} />
+        {activeFile && <MarkdownEditor activeFile={activeFile} />}
+
         {isDeleteConfirmationVisible && <DeleteConfirmation onClose={toggleDeleteConfirmation} />}
       </div>
     </div>
